9ee7507ecd4753d3901716d3283a3beeeeb71ab1
Nasseira <t.nasseira@gmail.com>
2016-11-18 11:26:50 +0100

    SuperHero

--- PokWarVel/App_Start/BundleConfig.cs
+++ PokWarVel/App_Start/BundleConfig.cs
@@ -21,11 +21,27 @@ namespace PokWarVel
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/js/audio.min.js",
+                      "~/Scripts/js/coundown-timer.js",
+                      "~/Scripts/js/gmaps.js",
+                      "~/Scripts/js/holder.js",
+                      "~/Scripts/js/html5shiv.js",
+                      "~/Scripts/js/jquery.countTo.js",
+                      "~/Scripts/js/jquery.fitvids.js",
+                      "~/Scripts/js/jquery.isotope.min.js",
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/animate.min.css",
+                      "~/Content/css/font-awesome.min.css",
+                      "~/Content/css/lightbox.css",
+                      "~/Content/css/main.css",
+                      "~/Content/css/prettyPhoto.css",
+                      "~/Content/css/responsive.css"
+                      ));
         }
     }
 }
